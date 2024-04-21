e2e0ff6d5e5ec28894b4e46874ff6b5af9e24688
rayyanmoin12 <166857521+rayyanmoin12@users.noreply.github.com>
2024-04-21 14:50:34 +0500

    my functional calculator

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("Welcome to Rayyan's calculator!");
+const ans = await inquirer.prompt([
+    {
+        type: "number",
+        name: "first_number",
+        message: "Write your first number: "
+    },
+    {
+        type: "number",
+        name: "second_number",
+        message: "Write your second number: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: "Pick your preferred operation.",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+if (ans.operator === "Addition") {
+    console.log(ans.first_number + ans.second_number);
+}
+else if (ans.operator === "Subtraction") {
+    console.log(ans.first_number - ans.second_number);
+}
+else if (ans.operator === "Multiplication") {
+    console.log(ans.first_number * ans.second_number);
+}
+else {
+    console.log(ans.first_number / ans.second_number);
+}
